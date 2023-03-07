9ceaede14ab7e0e3b715e9fb9f83f7b641bdd9fd
Polchik Nikita <polchik.nikita@gmail.com>
2023-03-07 14:54:01 +0300

    deleted bunch of useless packages

--- app2.js
+++ app2.js
@@ -1,13 +1,14 @@
 function towerBuilder(nFloors) {
     //result = nFloors * 2 - 1
     let arr = [];
-
-    for (let i = 0; i < nFloors * 2 - 1; i++) {
-        arr.push("*")
+    for (let i = 0; i < nFloors; i++) {
+        arr.push("")
+        for (let i = 0; i < nFloors * 2 - 1; i++) {
+            arr[arr.length - 1] += " "
+        }
     }
-    return arr.join("")
 
+    return arr
 }
 
-console.log(towerBuilder(5));
-console.log(towerBuilder(5));
+console.log(towerBuilder(3));
